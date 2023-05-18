49034289c4432d0e56f7966c722263dada980dc4
giuseppevignanello <peppe.vignanello@gmail.com>
2023-05-18 17:58:12 +0200

    add accordion

--- index.php
+++ index.php
@@ -39,8 +39,7 @@ include __DIR__ . "/db.php";
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
     <title>Computers</title>
 </head>
 
@@ -50,9 +49,7 @@ include __DIR__ . "/db.php";
         <nav class="navbar navbar-expand-lg bg-light">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                    aria-expanded="false" aria-label="Toggle navigation">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -64,8 +61,7 @@ include __DIR__ . "/db.php";
                             <a class="nav-link" href="#">Link</a>
                         </li>
                         <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
-                                aria-expanded="false">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
                                 Dropdown
                             </a>
                             <ul class="dropdown-menu">
@@ -106,45 +102,65 @@ include __DIR__ . "/db.php";
 
                 <?php foreach ($computers as $computer) {
                 ?>
-                <div class="card m-3 p-2" style="width: 18rem;">
-                    <img class="card-img-top img-fluid" src="<?= $computer->getImagePath() ?>" alt="...">
-                    <div class="card-body">
-                        <h5 class="card-title"><?= $computer->getBrand() ?> <?= $computer->getBrand() ?> </h5>
-                        <h6 class="card-subtitle mb-2 text-muted "><?= $computer->getType() ?> </h6>
+                    <div class="card m-3 p-2" style="width: 18rem;">
+                        <img class="card-img-top img-fluid" src="<?= $computer->getImagePath() ?>" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?= $computer->getBrand() ?> <?= $computer->getBrand() ?> </h5>
+                            <h6 class="card-subtitle mb-2 text-muted "><?= $computer->getType() ?> </h6>
+                            <div class="accordion" id="accordionExample">
+                                <div class="accordion-item">
+                                    <h2 class="accordion-header" id="headingOne">
+                                        <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                                            Details
+                                        </button>
+                                    </h2>
+                                    <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                                        <div class="accordion-body">
+                                            <ul class="list-group">
+                                                <li class="list-group-item"> <strong> Monitor </strong>
+                                                    <?= $computer->monitor->name ?>
+                                                    <?= $computer->monitor->inch ?>
+                                                </li>
+                                                <li class="list-group-item"> <strong> Keyboard </strong>
+                                                    <?= $computer->keyboard->name ?><?= $computer->keyboard->language ?>
+                                                </li>
+                                                <li class="list-group-item"><strong> RAM </strong><?= $computer->getRAM() ?>
+                                                    GB </li>
+                                                <li class="list-group-item"><strong> Memory
+                                                    </strong><?= $computer->getMemory() ?></li>
+                                                <li class="list-group-item"><strong> CPU </strong><?= $computer->getCPU() ?>
+                                                </li>
+                                                <li class="list-group-item"><strong> Weight
+                                                    </strong><?= $computer->getWeight() ?> Kg
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <strong>
+                                                        <?= $computer->getType() === "Desktop" ? "MotherBoard " : "Battery " ?></strong><?= $computer->getType() === "Desktop" ? $computer->getMotherboard() : $computer->getBattery() ?>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <strong>
+                                                        <?= $computer->getType() === "Desktop" ? "Graphics Card" : "TouchPad " ?></strong><?= $computer->getType() === "Desktop" ? $computer->getGraphicCard() : $computer->getTouchpad() ?>
+                                                </li>
+                                                <li class="list-group-item">
+                                                    <strong>
+                                                        <?= $computer->getType() === "Desktop" ? "Power Supply" : "Laptop Chassis " ?></strong><?= $computer->getType() === "Desktop" ? $computer->getPowerSupply() : $computer->getLaptopChassis() ?>
+                                                </li>
 
-                        <ul class="list-group">
-                            <li class="list-group-item"> <strong> Monitor </strong> <?= $computer->monitor->name ?>
-                                <?= $computer->monitor->inch ?>
-                            </li>
-                            <li class="list-group-item"> <strong> Keyboard </strong>
-                                <?= $computer->keyboard->name ?><?= $computer->keyboard->language ?>
-                            </li>
-                            <li class="list-group-item"><strong> RAM </strong><?= $computer->getRAM() ?> GB </li>
-                            <li class="list-group-item"><strong> Memory </strong><?= $computer->getMemory() ?></li>
-                            <li class="list-group-item"><strong> CPU </strong><?= $computer->getCPU() ?></li>
-                            <li class="list-group-item"><strong> Weight </strong><?= $computer->getWeight() ?> Kg </li>
-                            <li class="list-group-item">
-                                <strong>
-                                    <?= $computer->getType() === "Desktop" ? "MotherBoard " : "Battery " ?></strong><?= $computer->getType() === "Desktop" ? $computer->getMotherboard() : $computer->getBattery() ?>
-                            </li>
-                            <li class="list-group-item">
-                                <strong>
-                                    <?= $computer->getType() === "Desktop" ? "Graphics Card" : "TouchPad " ?></strong><?= $computer->getType() === "Desktop" ? $computer->getGraphicCard() : $computer->getTouchpad() ?>
-                            </li>
-                            <li class="list-group-item">
-                                <strong>
-                                    <?= $computer->getType() === "Desktop" ? "Power Supply" : "Laptop Chassis " ?></strong><?= $computer->getType() === "Desktop" ? $computer->getPowerSupply() : $computer->getLaptopChassis() ?>
-                            </li>
+                                            </ul>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
 
-                        </ul>
+                        </div>
                     </div>
-
-
-                </div>
                 <?php } ?>
             </div>
         </div>
     </main>
 </body>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+</script>
 
 </html>
